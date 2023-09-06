0484a4b766c5a36211cbf868cba9617f40e5ca8b
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-09-06 10:19:13 +0200

    Signin fix

---
+++ client/src/app/auth/signup/page.tsx
@@ -0,0 +1,8 @@
+export default async function SignupPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center space-y-5 p-24">
+      {/* <Signin /> */}
+      Signup
+    </main>
+  );
+}
