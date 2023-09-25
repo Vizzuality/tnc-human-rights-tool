57db6bbc454865031ab11a58e7eb53f703647467
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-09-25 18:19:24 +0200

    Project rik: title and description

--- client/src/app/projects/[id]/project-risk/[ctxId]/page.tsx
+++ client/src/app/projects/[id]/project-risk/[ctxId]/page.tsx
@@ -1,26 +1,32 @@
 // import { ProjectsDetailPageProps } from "@/app/projects/[id]/page";
 
+import parse from "html-react-parser";
+
+import { getContextualRisksId } from "@/types/generated/contextual-risk";
+
+import { ProjectsDetailPageProps } from "@/app/projects/[id]/page";
+
 import ProjectsDetailContent from "@/containers/projects/detail/content";
 import ProjectRiskForm from "@/containers/projects/detail/forms/project-risk";
 import ProjectsDetailTitle from "@/containers/projects/detail/title";
 
-// interface ProjectsDetailProjectRiskIdProps {
-//   params: {
-//     ctxId: string;
-//   } & ProjectsDetailPageProps["params"];
-// }
+interface ProjectsDetailProjectRiskIdProps {
+  params: {
+    ctxId: string;
+  } & ProjectsDetailPageProps["params"];
+}
 
-export default async function ProjectsDetailProjectRiskIdPage() {
+export default async function ProjectsDetailProjectRiskIdPage({
+  params: { ctxId },
+}: ProjectsDetailProjectRiskIdProps) {
+  const CTX_RISK = await getContextualRisksId(+ctxId);
+
   return (
     <ProjectsDetailContent>
-      <ProjectsDetailTitle>Contextual Risk Name</ProjectsDetailTitle>
+      <ProjectsDetailTitle>{CTX_RISK.data?.attributes?.title}</ProjectsDetailTitle>
 
       <div>
-        <div className="prose -mt-5">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident tempore maiores libero
-          vitae in nulla officiis dignissimos, error non enim natus culpa, facere nemo accusamus ea
-          repellendus vero. Ipsa, quae?
-        </div>
+        <div className="prose -mt-5">{parse(CTX_RISK.data?.attributes?.description ?? "")}</div>
       </div>
 
       <ProjectRiskForm />
