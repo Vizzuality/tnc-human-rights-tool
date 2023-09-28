7f4c9e3dca1df8ac4c1a285a1ceabe249f78b2f0
Tiago Garcia <tiagojsag@gmail.com>
2023-09-28 08:25:53 +0100

    Fix webshot entrypoint script

--- webshot/entrypoint.sh
+++ webshot/entrypoint.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 set -e
 
-case "$1" in
-    develop)
-        echo "Running Development Server"
-        exec grunt --gruntfile app/Gruntfile.js | bunyan
-        ;;
-    test)
-        echo "Running Test"
-        exec yarn test
-        ;;
-    start)
-        echo "Running Start"
-        exec yarn start
-        ;;
-    *)
-        exec "$@"
-esac
+echo "Running Webshot Server"
+exec yarn start
