a7b7bf5b15f15d6fcc62974718d713bfb5b1518f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-10-04 10:01:22 +0200

    Forgot and reset pasword scaffold

--- client/src/containers/reset-password/index.tsx
+++ client/src/containers/reset-password/index.tsx
@@ -2,12 +2,12 @@
 
 import { useForm } from "react-hook-form";
 
-import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signIn } from "next-auth/react";
 import { z } from "zod";
+
+import { usePostAuthResetPassword } from "@/types/generated/users-permissions-auth";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -21,20 +21,31 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  email: z.string().email({ message: "Please enter your email address" }),
-  password: z.string().nonempty({ message: "Please enter your password" }),
-});
+const formSchema = z
+  .object({
+    password: z.string().nonempty({ message: "Please enter your password" }).min(6, {
+      message: "Please enter a password with at least 6 characters",
+    }),
+    passwordConfirmation: z
+      .string()
+      .nonempty({ message: "Please enter your confirmed password" })
+      .min(6, { message: "Please enter a password with at least 6 characters" }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  });
 
-export default function Signin() {
+export default function ResetPassword() {
   const searchParams = useSearchParams();
+  const signupMutation = usePostAuthResetPassword();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "",
       password: "",
+      passwordConfirmation: "",
     },
   });
 
@@ -42,40 +53,40 @@ export default function Signin() {
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    signIn("credentials", {
-      email: values.email,
-      password: values.password,
-      callbackUrl: searchParams.get("callbackUrl") ?? "/projects",
-    });
+
+    // 3. Submit the form.
+    signupMutation.mutate(
+      {
+        data: {
+          ...values,
+          code: searchParams.get("code") ?? "",
+        },
+      },
+      {
+        onSuccess: () => {
+          console.info("success");
+        },
+        onError: (error) => {
+          console.error(error);
+        },
+      },
+    );
   }
 
   return (
     <Card className="min-w-[380px]">
       <CardHeader>
-        <CardTitle>Sign in</CardTitle>
+        <CardTitle>Reset password</CardTitle>
         {!!searchParams.get("error") && (
-          <div className="rounded-md bg-destructive p-3 text-sm text-destructive-foreground">
-            Invalid username or password. Please try again.
+          <div className="rounded-md bg-destructive/90 p-3 text-sm text-destructive-foreground">
+            {searchParams.get("error")}
           </div>
         )}
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-10">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
             <fieldset className="space-y-2">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
               <FormField
                 control={form.control}
                 name="password"
@@ -89,25 +100,24 @@ export default function Signin() {
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="passwordConfirmation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </fieldset>
-
             <div className="space-y-3">
               <Button className="w-full" type="submit">
-                Sign in
+                Reset password
               </Button>
-              <p className="text-center text-sm">
-                {"Don't"} have an account?{" "}
-                <Link className="text-primary hover:underline" href="/auth/signup">
-                  Sign up
-                </Link>{" "}
-                instead.
-              </p>
-              <p className="text-center text-sm">
-                Forgot you password?{" "}
-                <Link className="text-primary hover:underline" href="/auth/forgot-password">
-                  Reset password
-                </Link>
-              </p>
             </div>
           </form>
         </Form>
